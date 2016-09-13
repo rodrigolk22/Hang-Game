2755842a500021540fff729ed632e940d3cb34a1
Jordan Kobellarz <jordan.kobellarz@gmail.com>
2016-09-13 19:14:26 -0300

    adicionado gerador de palavras em ingles bem completo

--- game/helpers/generator.js
+++ game/helpers/generator.js
@@ -1,32 +1,11 @@
-var words = [
-    'dinossauro',
-    'galinha',
-    'ovo',
-    'planta',
-    'pessoa',
-    'jardim'
-];
-
-/**
- * A random number between min inclusive and max inclusive
- * @param min
- * @param max
- * @returns {*}
- */
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+var randomWords = require('random-words');
 
 /**
  * Generate a random word
  * @returns {string}
  */
 var generate = function () {
-    var max = words.length - 1;
-    var index = getRandomIntInclusive(0, max);
-    return words[index];
+    return randomWords();
 };
 
 module.exports = {
